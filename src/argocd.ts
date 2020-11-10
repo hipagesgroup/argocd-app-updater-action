3d7f0999e4fd0f9cbc55036bdfab6a99c853da56
Enrico Stahn <enrico.stahn@gmail.com>
2020-11-10 13:59:40 +1100

    feat: initial version

---
+++ src/argocd.ts
@@ -0,0 +1,86 @@
+import yaml from 'js-yaml'
+import axios, { AxiosResponse } from 'axios'
+import axiosRetry from 'axios-retry'
+import compareVersions, { compare } from 'compare-versions'
+
+export type Application = {
+    spec: {
+        source: {
+            repoURL: string
+            chart: string
+            targetRevision: string
+            // TODO: name ok?
+            newTargetRevision: string
+        }
+    }
+}
+
+export type VersionInfo = {
+    type: string // semver, git
+    revisionFrom: string
+    revisionTo: string
+    error: string
+}
+
+// export function readFromYAML(yaml: string): Application {
+//     let app: Application = <Application>{};
+//     return app
+// }
+
+export async function readFromString(data: string): Promise<Application> {
+    // TODO: handle failures, e.g. if yaml cant be used regex parse the structure
+    const app: Application = yaml.safeLoad(data) as Application
+
+    // Remove trailing slash
+    if (app.spec.source.repoURL.substr(-1) == '/') {
+        app.spec.source.repoURL = app.spec.source.repoURL.slice(0, -1);
+    }
+
+    // TODO: Should this be done here?
+    app.spec.source.newTargetRevision = await newerRevision(app)
+
+    return app
+}
+
+export async function newerRevision(app: Application): Promise<string> {
+    const index = await helmRepoIndex(`${app.spec.source.repoURL}/index.yaml`)
+    const newTargetRevision = index.entries[app.spec.source.chart][0].version
+
+      // Verify that both targetRevision and newTargetRevision are semver compliant.
+      if (!compareVersions.validate(newTargetRevision) || !compareVersions.validate(app.spec.source.targetRevision)) {
+        return ''
+      }
+
+      // Continue only if a new version is available
+      if (compareVersions.compare(newTargetRevision, app.spec.source.targetRevision, '<=')) {
+        return ''
+      }
+
+    return newTargetRevision
+}
+
+const HelmChartRepositories: Record<string, Promise<HelmChartRepositoryIndex>> = {}
+
+type HelmChartRepositoryIndex = {
+    entries: Record<string, HelmChartRepositoryChartVersion[]>
+}
+
+type HelmChartRepositoryChartVersion = {
+    version: string
+}
+
+export async function helmRepoIndex(url: string): Promise<HelmChartRepositoryIndex> {
+    if (HelmChartRepositories[url]) {
+        return HelmChartRepositories[url]
+    }
+
+    HelmChartRepositories[url] = (async (): Promise<HelmChartRepositoryIndex> => {
+        axiosRetry(axios, { retries: 3, retryCondition: axiosRetry.isRetryableError })
+
+        const result: AxiosResponse = await axios.get(url)
+
+        return yaml.safeLoad(result.data) as HelmChartRepositoryIndex
+    })()
+
+    return HelmChartRepositories[url]
+}
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,152 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import yaml from 'js-yaml'
+import axios from 'axios'
+import compareVersions, { compare } from 'compare-versions'
+import axiosRetry from 'axios-retry'
+import multimatch from 'multimatch'
+import md5 from 'md5'
+import * as argocd from './argocd'
+// import {wait} from './wait'
+
+// Resources
+// - https://github.com/octokit/rest.js/issues/845
+// - https://github.com/octokit/rest.js/issues/1308
+
+// type ArgoApplication = {
+//   spec: {
+//     source: {
+//       repoURL: string
+//       chart: string
+//       targetRevision: string
+//     }
+//   }
+// }
+
+// type HelmChartRepositoryIndex = {
+//   entries: Record<string, HelmChartRepositoryChartVersion[]>
+// }
+
+// type HelmChartRepositoryChartVersion = {
+//   version: string
+// }
+
+// 1. Read from default branch to find if ArgoCD files exists
+// 2. If yes, check if ArgoCD app is helm chart and requires update
+// 3. If yes, create a branch for that app and update the file (e.g. argocd-app-update/<filename-hash>)
+// 4. rinse repeat
+
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    // const token = core.getInput("github-token", { required: true });
+    const octokit = github.getOctokit(process.env.GITHUB_TOKEN || '')
+    const org = "hipagesgroup"
+    const repo = "salesforce-syncer"
+    const filePatterns = ['.argocd**.yml']
+    const baseBranchName = "master"
+    const headBranchNamePrefix = "argocd-app-update"
+    const ctx = { owner: org, repo: repo }
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // Find if the repo has files that match the defined pattern
+    const { data: refData } = await octokit.git.getRef({ ...ctx, ref: `heads/${baseBranchName}` })
+    const getTreeResponse = await octokit.git.getTree({ ...ctx, tree_sha: refData.object.sha })
 
-    core.setOutput('time', new Date().toTimeString())
+    const treeItems = (getTreeResponse.data?.tree || []).filter(function (element, index, array) {
+      return multimatch([element.path], filePatterns).length > 0
+    })
+
+    // Let's get to work and check those files
+    for (const treeItem of treeItems) {
+      core.debug(`Processing file: ${treeItem.path}`)
+
+      const headBranchName = `${headBranchNamePrefix}-${md5(treeItem.path)}`
+
+      // Determine if the branch exists
+      let branchExists = false
+      try {
+        await octokit.git.getRef({ ...ctx, ref: `heads/${headBranchName}` })
+        branchExists = true
+      } catch (error) {
+        // Bubble up if its not branch not found error.
+        if (error.status != 404) {
+          throw error
+        }
+      }
+
+      // Determine if update is required
+      const { data: file } = await octokit.repos.getContent({
+        ...ctx,
+        ref: branchExists ? `heads/${headBranchName}` : `heads/${baseBranchName}`,
+        path: treeItem.path,
+      })
+
+      const fileContent = Buffer.from(file.content, 'base64').toString('ascii')
+      const app = await argocd.readFromString(fileContent)
+
+      // If update is not required, continue with the next file
+      if (!app.spec.source.newTargetRevision) {
+        continue
+      }
+
+      // Update required, create branch if it doesn't exist
+      if (!branchExists) {
+        core.debug(`Branch missing, creating branch ${headBranchName}`)
+        const { data: refData } = await octokit.git.getRef({ ...ctx, ref: `heads/${baseBranchName}` })
+        await octokit.git.createRef({ ...ctx, ref: `refs/heads/${headBranchName}`, sha: refData.object.sha })
+      }
+
+      // Get file from head branch, in case it did exists
+      const { data: file2 } = await octokit.repos.getContent({
+        ...ctx,
+        ref: `heads/${headBranchName}`,
+        path: treeItem.path,
+      })
+
+      let fileContent2 = Buffer.from(file2.content, 'base64').toString('ascii')
+
+      // core.debug(`Found ${file.path} containing chart ${app.spec.source.chart} with version ${app.spec.source.targetRevision}`)
+      // core.debug(`Fetching repo index from ${app.spec.source.repoURL}/index.yaml for chart ${app.spec.source.chart}`)
+      core.debug(`Latest version for chart ${app.spec.source.chart} in index is ${app.spec.source.newTargetRevision}`)
+
+        // core.debug(`Skipping chart ${app.spec.source.chart}, no newer version available.`)
+
+      fileContent2 = fileContent2.replace(`targetRevision: ${app.spec.source.targetRevision}`, `targetRevision: ${app.spec.source.newTargetRevision}`)
+
+      core.debug(`build(chart): bump ${app.spec.source.chart} from ${app.spec.source.targetRevision} to ${app.spec.source.newTargetRevision}`)
+      await octokit.repos.createOrUpdateFileContents({
+        ...ctx,
+        path: file2.path,
+        message: `build(chart): bump ${app.spec.source.chart} from ${app.spec.source.targetRevision} to ${app.spec.source.newTargetRevision}`,
+        content: Buffer.from(fileContent2, 'ascii').toString('base64'),
+        sha: file2.sha,
+        branch: `refs/heads/${headBranchName}`
+      })
+
+      let pullRequestBody = `
+Bumps chart \`${app.spec.source.chart}\` from \`${app.spec.source.targetRevision}\` to \`${app.spec.source.newTargetRevision}\`.
+
+**⚠️ Important**
+
+Please ensure you have done your due diligence before merging. The checklist below will provide some pointers:
+
+- [ ] Check the diff in ArgoCD and possibly adjust helm values if necessary
+- [ ] Check the release log of the chart for breaking changes
+      `.trim()
+
+      core.debug(`Creating pull request`)
+      await octokit.pulls.create({
+        owner: org,
+        repo: repo,
+        title: `build(chart): bump ${app.spec.source.chart} from ${app.spec.source.targetRevision} to ${app.spec.source.newTargetRevision}`,
+        head: `refs/heads/${headBranchName}`,
+        base: `refs/heads/${baseBranchName}`,
+        body: pullRequestBody,
+        maintainer_can_modify: true
+      })
+    }
   } catch (error) {
+    core.debug(error)
     core.setFailed(error.message)
   }
 }
